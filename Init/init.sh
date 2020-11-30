04d560b34bf1ceb204270fc15952232b20e31299
psp123123 <17600899296@126.com>
2020-11-30 16:57:24 +0800

    Update init.sh

--- Init/init.sh
+++ Init/init.sh
@@ -1,33 +1,36 @@
+#########################################################################
+# File Name: init.sh
+# Created Time: Mon 30 Nov 2020 04:34:12 PM CST
+#########################################################################
 #!/bin/bash
 
 function yum_install(){
-	yum install -y net-tools vim lrzsz tree screen lsof tcpdump wget chrony telnet unzip ntpdate
+        yum install -y net-tools vim lrzsz tree screen lsof tcpdump wget chrony telnet unzip ntpdate
 }
 
 function epel(){
-	wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
-	mv /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.backup
-	mv /etc/yum.repos.d/epel-testing.repo /etc/yum.repos.d/epel-testing.repo.backup
-	wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
-	yum makecache
+        wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+        mv /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel.repo.backup
+        mv /etc/yum.repos.d/epel-testing.repo /etc/yum.repos.d/epel-testing.repo.backup
+        wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
+        yum makecache
 }
 
-function locale(){
-	cat >> /etc/environment << EOF
-	LANG=en_US.utf-8
-	LC_ALL=en_US.utf-8
-	EOF
-	source /etc/environment
-	echo 'LANG="en_US.utf-8"' > /etc/locale.conf
-	source /etc/locale.conf
-	locale
+function _locale(){
+        cat >> /etc/environment << EOF
+        LANG=en_US.utf-8
+        LC_ALL=en_US.utf-8
 EOF
+        source /etc/environment
+        echo 'LANG="en_US.utf-8"' > /etc/locale.conf
+        source /etc/locale.conf
+        locale
 }
 
 function utc(){
-	ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+        ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 }
-	
+
 
 function ulimit(){
 cat >> /etc/security/limits.conf<< EOF
@@ -35,18 +38,26 @@ cat >> /etc/security/limits.conf<< EOF
 * hard nofile 65535
 EOF
 }
+function limit(){
+        sed -i 's#SELINUX=disabled#SELINUX=entenforce#g' /etc/selinux/config 
+        systemctl stop firewalld
+        systemctl disable firewalld
+}
 
 #while true:
 #  do
-echo '
->> 0: 自动优化（下面各项都会优化）
->> 1: 修改epel源为阿里云
->> 2: 添加必要的工具：net-tools vim lrzsz tree screen lsof tcpdump wget chrony telnet unzip ntpdate
->> 3: 修改字符集
->> 4: 修改时区
->> 5: 修改limit值
->> 6: 输入6退出脚本
-'
+echo -e "\e[1;31m
+>> 0: 自动优化（下面各项都会优化）\e[0m"
+echo -e "\e[1;32m>> 1: 修改epel源为阿里云\e[0m"
+echo -e "\e[1;32m>> 2: 添加必要的工具：net-tools vim lrzsz tree screen lsof tcpdump wget chrony telnet unzip ntpdate\e[0m"
+echo -e "\e[1;32m>> 3: 修改字符集\e[0m"
+echo -e "\e[1;32m>> 4: 修改时区\e[0m"
+echo -e "\e[1;32m>> 5: 修改limit值\e[0m"
+echo -e "\e[1;32m>> 6: 关闭linux的限制（Selinux/firewalld）\e[0m"
+echo -e "\e[1;34m
+>> q: 输入q退出脚本
+\e[0m"
+
 #read -p '请输入需要优化内容：' choice
 while true
   do
@@ -57,6 +68,10 @@ while true
           then
             break
             #echo '输入正确'
+        elif [ "$choice" == "q" ]
+          then
+            echo -e "\e[1;31m退出脚本 ^v^\e[0m"
+            exit 1
         else
           echo "[ $choice ]字符输入错误,请重新输入"
         fi
@@ -68,7 +83,9 @@ while true
         yum_install  > /dev/null 2>&1
         locale  > /dev/null 2>&1
         utc  > /dev/null 2>&1
-	echo 'done'
+        _ulimit > /dev/null 2>&1
+        limit > /dev/null 2>&1
+        echo 'done'
         ;;
       1)
         epel  > /dev/null 2>&1
@@ -79,7 +96,7 @@ while true
         echo done
         ;;
       3)
-        locale  > /dev/null 2>&1
+        _locale  > /dev/null 2>&1
         echo done
         ;;
       4)
@@ -91,7 +108,10 @@ while true
         echo done
         ;;
       6)
+        limit
+        ;;
+      q)
         exit
-	;;
+        ;;
     esac
 done
