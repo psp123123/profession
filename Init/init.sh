b1a707e673e6d103130b90194e266f31dccea252
psp123123 <17600899296@126.com>
2021-05-20 15:44:37 +0800

    Update init.sh
    bugfix

--- Init/init.sh
+++ Init/init.sh
@@ -37,6 +37,7 @@ cat >> /etc/security/limits.conf<< EOF
 * soft nofile 65535
 * hard nofile 65535
 EOF
+ulimit -n 655350
 }
 function limit(){
         sed -i 's#SELINUX=enforcing#iSELINUX=disabled#g' /etc/selinux/config 
